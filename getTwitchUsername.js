557d4d0c9e120c75c2c18bcda9bee26ee4b7b27a
808raf <102700868+808raf@users.noreply.github.com>
2024-05-02 16:57:45 -1000

    Revert "First commit"
    This reverts commit 583f7eb9df474a1cbb19563de5760eb71fff0cb8.

--- getTwitchUsername.js
+++
@@ -1,8 +0,0 @@
-const getTwitchUsername = async (page) => {
-  // Extract the h1 with class="tw-title"
-  const twitchTitle = await page.$eval("h1.tw-title", (h1) => h1.textContent);
-
-  return twitchTitle;
-};
-
-export default getTwitchUsername;
--- twitchUsernames.js
+++
@@ -1,9 +0,0 @@
-// Put the twitch names you want to scrape here
-const twitchUsernames = [
-  "username1",
-  "anothername",
-  "yetanothername",
-  "username4",
-];
-
-export default twitchUsernames;
